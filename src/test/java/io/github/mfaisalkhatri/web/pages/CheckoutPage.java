0b9c27eb4c6d0efe8bdfa162341cf09e13b93074
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2024-10-02 15:53:39 +0300

    test implementation of of scenario 4 wip

--- src/test/java/io/github/mfaisalkhatri/web/pages/CheckoutPage.java
+++ src/test/java/io/github/mfaisalkhatri/web/pages/CheckoutPage.java
@@ -1,8 +1,75 @@
 package io.github.mfaisalkhatri.web.pages;
 
+import io.github.boykaframework.builders.Locator;
+import org.openqa.selenium.By;
+
 public class CheckoutPage {
 
     public CheckoutPage(){
 
     }
+
+    private Locator productName = Locator.buildLocator()
+            .name("Product Name in Shopping cart")
+            .web(By.cssSelector("td.text-left:nth-child(2) > a"))
+            .build();
+
+    private Locator productPrice = Locator.buildLocator()
+            .name("Product Total Price in Cart")
+            .web(By.cssSelector("td.text-right:nth-child(5)"))
+            .build();
+
+    private Locator firstNameField = Locator.buildLocator()
+            .name("First Name Field")
+            .web(By.id("input-payment-firstname"))
+            .build();
+
+    private Locator companyField = Locator.buildLocator()
+            .name("Company Field")
+            .web(By.id("input-payment-company"))
+            .build();
+
+    private Locator addressOneField = Locator.buildLocator()
+            .name("Address One Field")
+            .web(By.id("input-payment-address-1"))
+            .build();
+
+    private Locator cityField = Locator.buildLocator()
+            .name("City Field")
+            .web(By.id("input-payment-city"))
+            .build();
+
+    private Locator postCodeField = Locator.buildLocator()
+            .name("Post Code Field")
+            .web(By.id("input-payment-postcode"))
+            .build();
+
+    private Locator countryField = Locator.buildLocator()
+            .name("Country Field")
+            .web(By.id("input-payment-country"))
+            .build();
+
+    private Locator regionField = Locator.buildLocator()
+            .name("Region Field")
+            .web(By.id("input-payment-zone"))
+            .build();
+
+    private Locator commentBox = Locator.buildLocator()
+            .name("Comment box")
+            .web(By.id("input-comment"))
+            .build();
+
+    private Locator termsAndConditionsCheckbox = Locator.buildLocator()
+            .name("Terms and Conditions Checkbox")
+            .web(By.id("div.custom-control:nth-child(6)"))
+            .build();
+
+    private Locator continueBtn = Locator.buildLocator()
+            .name("Terms and Conditions Checkbox")
+            .web(By.cssSelector("button#button-save"))
+            .build();
+
+
+
 }
+
